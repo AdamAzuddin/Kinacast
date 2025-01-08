f262924f12eb6fac333a77fc98bb8e0b1a9cc3f0
Adam Azuddin <05adamaz@gmail.com>
2025-01-08 16:29:55 +0800

    Updates

--- constants/NavItems.js
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-    { name: 'Home', href: '/' },
-    { name: 'Episodes', href: '/episodes' },
-    { name: 'Speakers', href: '/speakers' },
-    { name: 'Crews', href: '/crews' },
-    { name: 'Reach Out', href: '/contact-us' },
-  ]
