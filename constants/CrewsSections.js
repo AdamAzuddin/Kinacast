c4e60a50d3fa731646498b88af7ce7b30132d411
Adam Azuddin <05adamaz@gmail.com>
2025-01-08 15:00:01 +0800

    modified the crews page to align using flexbox and create crews sections constant

---
+++ constants/CrewsSections.js
@@ -0,0 +1,85 @@
+export const sections = [
+  {
+    title: "High Committee",
+    members: [
+      { name: "Afzal", title: "Director", image: "/high_comm/Afzal.JPG" },
+      {
+        name: "Sayyid",
+        title: "Vice Director",
+        image: "/high_comm/Sayyid.jpeg",
+      },
+      { name: "Eunice", title: "Secretary", image: "/high_comm/Eunice.JPG" },
+      {
+        name: "Hidnee",
+        title: "Vice Secretary",
+        image: "/high_comm/Hidnee.JPG",
+      },
+      { name: "Zaf", title: "Treasurer", image: "/high_comm/Zaf.jpeg" },
+    ],
+  },
+  {
+    title: "Multimedia and Publicity",
+    members: [
+      {
+        name: "Farish",
+        title: "Head of Department",
+        image: "/multimedia/Farish.JPG",
+      },
+      { name: "Auni", image: "/multimedia/Auni.JPG" },
+      { name: "Danish", image: "/multimedia/Danish.jpeg" },
+      { name: "Fariey", image: "/multimedia/Fariey.JPG" },
+      { name: "Ifhami", image: "/multimedia/Ifhami.JPG" },
+      { name: "Zahra", title: "Advisor", image: "/multimedia/Zahra.jpg" },
+    ],
+  },
+  {
+    title: "Logistics, Technical, and Video",
+    members: [
+      {
+        name: "Arif",
+        title: "Head of Department",
+        image: "/logitech_vid/arif.JPG",
+      },
+      { name: "Afiq Jasmy", image: "/logitech_vid/afiq_jasmy.JPG" },
+      { name: "Alief", image: "/logitech_vid/alief.JPG" },
+      { name: "Hana", image: "/logitech_vid/hana.JPG" },
+      { name: "Kisshore", image: "/logitech_vid/kisshore.JPG" },
+      { name: "Iman", image: "/logitech_vid/iman.jpeg" },
+    ],
+  },
+  {
+    title: "Creative & Content",
+    members: [
+      {
+        name: "Afiq",
+        title: "Head of Department",
+        image: "/content_creative/afiq.jpeg",
+      },
+      { name: "Adib", image: "/content_creative/adib.jpeg" },
+      { name: "Aiman", image: "/content_creative/aiman.JPG" },
+      { name: "Aqlan", image: "/content_creative/aqlan.JPG" },
+      { name: "Shi Yee", image: "/content_creative/shi_yee.JPG" },
+      { name: "Erika", image: "/content_creative/erika.jpeg" },
+      { name: "Mar", image: "/content_creative/mar.JPG" },
+      { name: "Rashman", image: "/content_creative/rashman.JPG" },
+      {
+        name: "Shi Yong",
+        title: "Advisor",
+        image: "/content_creative/shi_yong.JPG",
+      },
+    ],
+  },
+  {
+    title: "Sponsorship and Public Relations",
+    members: [
+      {
+        name: "Harshini",
+        title: "Head of Department",
+        image: "/sponsor_public_rel/Harshini.JPG",
+      },
+      { name: "Nabilah", image: "/sponsor_public_rel/Nabilah.JPG" },
+      { name: "Fara", image: "/sponsor_public_rel/Fara.jpeg" },
+      { name: "Adam", title: "Advisor", image: "/sponsor_public_rel/Adam.jpg" },
+    ],
+  },
+];
